97a69dfe11e307d4a88fef62f2be795eb6621fe5
VitaxaOv <vit2013aly@mail.ru>
2022-11-21 20:27:38 +0300

    init project

---
+++ Lesson1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//0. Демонстрация решения
+// Напишите программу, которая на вход принимает число
+// и выдаёт его квадрат (число умноженное на само себя).
+
+int number = int.Parse(Console.ReadLine());
+
+int quarter = number * number;
+
+Console.WriteLine(quarter);
