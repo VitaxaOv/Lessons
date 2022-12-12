3f18b77b7f2bdd7bd22cd410a319a88217322772
VitalyOv <vit2013aly@mail.ru>
2022-12-12 20:55:54 +0300

    Add example project 7

--- Common/Helper.cs
+++ Common/Helper.cs
@@ -1,11 +1,37 @@
-﻿using System;
-namespace Common;
+﻿namespace Common;
 
 // dotnet new classlib --name Common
 // dotnet add reference /Users/a2/Lessons/Common/Common.csproj
 
 public static class Helper
 {
+    public static void PrintArray(int[,] array)
+    {
+        for (var i = 0; i < array.GetLength(0); i++)
+        {
+            for (var j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static int[,] CreateRandom2DArray(int countOfRows, int countOfColumns)
+    {
+        Random random = new Random();
+        int[,] array = new int[countOfRows, countOfColumns];
+
+        for (var i = 0; i < array.GetLength(0); i++)
+        {
+            for (var j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = random.Next(-10, 11);
+            }
+        }
+        return array;
+    }
+
     public static void PrintArray(int[] array)
     {
         for (var i = 0; i < array.Length; i++)
@@ -33,7 +59,7 @@ public static class Helper
     {
         bool isParsed = int.TryParse(Console.ReadLine(), out int number);
 
-        if(isParsed)
+        if (isParsed)
         {
             return number;
         }
---
+++ Lesson7/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задача 46: Задайте двумерный массив размером m×n, 
+//заполненный случайными целыми числами.
+//m = 3, n = 4. 
+//1 4 8 19
+//5 -2 33 -2 
+//77 3 8 1
+using static Common.Helper;
+// 2D Array
+/*int[,] array = CreateRandom2DArray(3, 4);
+PrintArray(array);*/
+
+int[] array = new int[3] { 1, 3, 4 };
+
+string str = string.Join(",", array);
+Console.WriteLine(str);
+
+string strFormat = string.Format("Это означет {0}  равно {1} аывавыа {2}", GetSomething().Item1, "", false);
+string str2 = $"{GetSomething().Item1}";
+string str3 = "Это означает " +  GetSomething().Item1 + "  равно "+ "" + "аывавыа " + 2; //- не норм
+
+Console.WriteLine(strFormat);
+Console.WriteLine(str2);
+Console.WriteLine(str3);
+string.Concat("", "","");
+string.IsNullOrWhiteSpace();
+string.Compare("", "");
+str2.EndsWith("123");
+str2.StartsWith("123");
+str2.Contains("13213");
+str.Distinct();
+str.Remove(5,5);
+str.Trim("   asfsdf   ")
+
+
+
+
+
+
+
+
+
+// Tuples
+
+// (int,int) coordinates = GetSomething();
+// Console.WriteLine(coordinates.Item1);
+// Console.WriteLine(coordinates.Item2);
+
+(int,int) GetSomething()
+{
+    int a = 5;
+    int b = 10;
+    return (a,b);
+}
+
+
