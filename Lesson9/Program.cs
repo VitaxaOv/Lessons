8103d0066cae65405bfffe6fdf1876c8f4dc1ffb
VitalyOv <vit2013aly@mail.ru>
2022-12-19 20:26:04 +0300

    Add Lesson9

---
+++ Lesson9/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+for (var i = 0; i < 5; i++)
+{
+    Console.Write(" ! ");
+}
+
+Method();
+
+void Method(int i = 0)
+{
+    if (i < 5)
+    {
+        Console.Write(" ? ");
+        Method(i + 1);
+    }
+
+    return;
+}
